c26deb0f5318236a7409cd572b56820a4c30920c
daniel-990 <danielarango990@gmail.com>
2022-09-27 16:35:15 -0500

    se edita en archivo main

--- src/main/java/Proyecto_Citas/CitasMedicas.java
+++ src/main/java/Proyecto_Citas/CitasMedicas.java
@@ -6,10 +6,7 @@
 package Proyecto_Citas;
 
 import Vista_CitasMedicas.CitasMedicas_2;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import coneccion_db.coneccion;
 
 /**
  *
@@ -21,36 +18,7 @@ public class CitasMedicas {
         CitasMedicas_2 abrir = new CitasMedicas_2();
         abrir.setVisible(true);
 
-        //se inicial la coneccion a bases de datos
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_agendarcitasmedicas","root","");
-            String Query = "SELECT * FROM tbl_pacientes";
-            Statement stmt=con.createStatement();
-
-            ResultSet rs=stmt.executeQuery(Query);
-            System.out.println("Conectado a la base de datos");
-
-            while(rs.next()){
-                int id = rs.getInt("IdPaciente");
-                String nombre = rs.getString("NomPaciente");
-                String apellido = rs.getString("ApellPaciente");
-                String fechaNacimiento = rs.getString("FechaNacimientoPac");
-                String genero = rs.getString("GenPaciente");
-
-                //reusltados de la consulta
-                System.out.println(id);
-                System.out.println(nombre);
-                System.out.println(apellido);
-                System.out.println(fechaNacimiento);
-                System.out.println(genero);
-
-            }
-            stmt.close();
-        }catch(Exception e){
-            System.out.println(e);
-        }
-
+        //se inicia la coneccion a la base de datos
+        coneccion.consultasMysql();
     }
 }
